4fd22c4e2205cb806ebcd92a5464156c93e59c37
fredluggard <fredrick.ebuka33@gmail.com>
2024-06-13 22:34:11 +0100

    Updated Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,29 +1,38 @@
 import logo from "../assets/logo.png";
 import house from "../assets/house.png";
 import profile from "../assets/profile.png";
+import { Link } from "react-router-dom";
+
 const Login = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-loginBg">
-      <div className="flex flex-col items-center justify-center">
-        <img src={logo} alt="HealthKeep Logo" />
-        <h1 className="text-[39px] font-[500px] text-[#132838]">
-          Who&apos;s using Healthkeep?
-        </h1>
-        <p className="text-[20px] font-[500px]">Choose your profile</p>
-        <div className="w-[491px] flex flex-row items-center justify-around gap-1 mt-10">
-          <div className="flex flex-col items-center justify-center gap-3">
-            <h2>Organization</h2>
-            <span>
-              <img src={house} alt="House" />
-            </span>
-          </div>
+      <div className="flex flex-col gap-2 items-center justify-center">
+        <div className="flex flex-col items-center justify-center gap-2">
+          <img src={logo} alt="HealthKeep Logo" />
+          <h1 className="text-[39px] font-[500px] text-[#132838] mt-[60px] mb-[16px]">
+            Who&apos;s using Healthkeep?
+          </h1>
+          <p className="text-[20px] font-[500px]">Choose your profile</p>
+        </div>
 
-          <div className="flex flex-col items-center justify-center gap-3">
-            <h2>Patient</h2>
-            <span>
-              <img src={profile} alt="Profile" />
-            </span>
-          </div>
+        <div className="w-[491px] flex flex-row items-center justify-around gap-1 mt-10">
+          <Link to="/org-login">
+            <div className="flex flex-col items-center justify-center gap-3 w-[131px]">
+              <h2>Organization</h2>
+              <span>
+                <img src={house} alt="House" />
+              </span>
+            </div>
+          </Link>
+
+          <Link to="/user-login">
+            <div className="flex flex-col items-center justify-center gap-3">
+              <h2>Patient</h2>
+              <span>
+                <img src={profile} alt="Profile" />
+              </span>
+            </div>
+          </Link>
         </div>
       </div>
     </div>
---
+++ src/pages/Org-Login.jsx
@@ -0,0 +1,5 @@
+const OrgLogin = () => {
+  return <div>Org-Login</div>;
+};
+
+export default OrgLogin;
